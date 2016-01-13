484c582600936501c86238f0f9f6d0425c9e3976
Rick Pizzi <pizzi@leopardus.com>
2016-01-13 16:21:53 +0100

    new version of sampling script with permanent connection to DB

--- innodb_sampler/sampler.sh
+++ innodb_sampler/sampler.sh
@@ -1,30 +1,31 @@
-!/bin/bash
+#!/bin/bash
 #
 #
-#	takes a sample of SHOW ENGINE INNODB STATUS every $SLEEP seconds and stores it in files
+#	takes a sample of SHOW ENGINE INNODB STATUS every 10 seconds and stores it in files
 #	under $SAMPLEDIR for later use
 #	(run it in background with nohup)
-#	rpizzi@blackbirdit.com
+#	v2.0 	use coprocess and permanent connection to server
+#	riccardo.pizzi@rumbo.com
 #
-SAMPLEDIR=/mnt/log/samples
-HOSTS="server1:10.77.23.111 server2:10.77.44.22"
-SLEEP=10
+SAMPLEDIR=$HOME/sampling/data/$(hostname)
+SAMPLEFILE=/tmp/sampler.sample
 #
+echo -n "Password: "
+stty -echo
+read pass
+stty echo
+echo
+coproc mysqlc { script -c "mysql -ANrs -p$pass 2>/dev/null" /dev/null; }
 c=0
-ts=$(date +%H%M)
 while true
 do
 	month=$(date +%m)
 	day=$(date +%d)
-	for host in $HOSTS
-	do
-		hour=$(date +%H)
-		name=$(echo $host | cut -d":" -f 1)
-		addr=$(echo $host | cut -d":" -f 2)
-		folder=$SAMPLEDIR/$month/$day
-		[ ! -d $folder ] && mkdir -p $folder
-		echo "show engine innodb status" | mysql -r -h $addr >> $folder/${name}_${hour}.sample
-	done
-	wait
-	sleep $SLEEP
+	hour=$(date +%H)
+	folder=$SAMPLEDIR/$month/$day
+	[ ! -d $folder ] && mkdir -p $folder
+	echo "pager cat >> $folder/$hour.sample" >&${mysqlc[1]}
+	echo "show engine innodb status;" >&${mysqlc[1]}
+	sleep 10
 done
+
