19c9adf383ac558a9d2079b9ee124238b1395051
Rick Pizzi <pizzi@leopardus.com>
2016-08-24 16:28:48 +0200

    bug fix: new tables wouldn't show until 2 month old

--- table_growth/pz-table-growth.sh
+++ table_growth/pz-table-growth.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 #	statistics on table growth
-#	rpizzi@blackbirdit.com
+#	riccardo.pizzi@lastminute.com
 #
 #
-USER=growth
-PASS='gr0wth!'
+USER=
+PASS=
 #
 case "$1" in
 	'size') type=1;;
@@ -16,6 +16,7 @@ esac
 d7=$(echo "select id from growth where sample_date = date_sub(curdate(), interval 1 week) limit 1" | mysql -u$USER -p$PASS -ANr tablegrowth | wc -l)
 d14=$(echo "select id from growth where sample_date = date_sub(curdate(), interval 2 week) limit 1" | mysql -u$USER -p$PASS -ANr tablegrowth | wc -l)
 d30=$(echo "select id from growth where sample_date = date_sub(curdate(), interval 1 month) limit 1" | mysql -u$USER -p$PASS -ANr tablegrowth | wc -l)
+d60=$(echo "select id from growth where sample_date = date_sub(curdate(), interval 2 month) limit 1" | mysql -u$USER -p$PASS -ANr tablegrowth | wc -l)
 echo
 case $type in
 	1) echo "Top 20 tables by disk usage in GB";;
@@ -26,6 +27,17 @@ echo
 (
 	echo "select "
 	echo "	t0.server, t0.schema_name, t0.table_name, "
+	if [ $d60 -eq 1 ]
+	then
+		case $type in
+			1) echo -n "	t60.table_size";;
+			2) echo -n "	concat(truncate(100 - (t60.table_size * 100 / t0.table_size),1), '%')";;
+			3) echo -n "	t0.table_size - t60.table_size";;
+		esac
+	else
+		echo -n "	'N/A'"
+	fi
+	echo " as 2_months, "
 	if [ $d30 -eq 1 ]
 	then
 		case $type in
@@ -68,16 +80,12 @@ echo
 		1) echo "	t0.table_size as today ";;
 	esac
 	echo "from growth t0 "
-	echo "	left join growth t1 on t1.server = t0.server and t1.schema_name = t0.schema_name and t1.table_name = t0.table_name "
-	[ $d7 -eq 1 ] && echo "	left join growth t7 on t7.server = t0.server and t7.schema_name = t0.schema_name and t7.table_name = t0.table_name "
-	[ $d14 -eq 1 ] && echo "	left join growth t14 on t14.server = t0.server and t14.schema_name = t0.schema_name and t14.table_name = t0.table_name "
-	[ $d30 -eq 1 ] && echo "	left join growth t30 on t30.server = t0.server and t30.schema_name = t0.schema_name and t30.table_name = t0.table_name "
-	echo "where "
-		echo "	t1.sample_date =  date_sub(curdate(), interval 1 day) "
-	[ $d7 -eq 1 ] && echo "	and t7.sample_date =  date_sub(curdate(), interval 1 week) "
-	[ $d14 -eq 1 ] && echo "	and t14.sample_date =  date_sub(curdate(), interval 2 week) "
-	[ $d30 -eq 1 ] && echo "	and t30.sample_date =  date_sub(curdate(), interval 1 month) "
-	echo "	and t0.sample_date = curdate()"
+	echo "  left join growth t1 on t1.server = t0.server and t1.schema_name = t0.schema_name and t1.table_name = t0.table_name and t1.sample_date =  date_sub(curdate(), interval 1 day)"
+        [ $d7 -eq 1 ] && echo " left join growth t7 on t7.server = t0.server and t7.schema_name = t0.schema_name and t7.table_name = t0.table_name and t7.sample_date =  date_sub(curdate(), interval 1 week)"
+        [ $d14 -eq 1 ] && echo "        left join growth t14 on t14.server = t0.server and t14.schema_name = t0.schema_name and t14.table_name = t0.table_name and t14.sample_date =  date_sub(curdate(), interval 2 week)"
+        [ $d30 -eq 1 ] && echo "        left join growth t30 on t30.server = t0.server and t30.schema_name = t0.schema_name and t30.table_name = t0.table_name and t30.sample_date =  date_sub(curdate(), interval 1 month)"
+        [ $d60 -eq 1 ] && echo "        left join growth t60 on t60.server = t0.server and t60.schema_name = t0.schema_name and t60.table_name = t0.table_name and t60.sample_date =  date_sub(curdate(), interval 2 month)"
+        echo "where t0.sample_date = curdate()"
 	case $type in 
 		2) echo "order by truncate(100 - (t1.table_size * 100 / t0.table_size), 1) desc limit 20;";;
 		*) echo "order by 1_day desc limit 20;";;
