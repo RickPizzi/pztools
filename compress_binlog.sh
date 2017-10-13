bc9a9bb06f149ee7d38b7fc8e53b1f65d2b74a6e
Rick Pizzi <pizzi@leopardus.com>
2017-10-13 10:58:02 +0200

    various fixes

--- compress_binlog.sh
+++ compress_binlog.sh
@@ -13,21 +13,39 @@ REPL_USER=
 REPL_PASSWORD=
 MASTER_HOST=localhost
 #
+tmpf=/tmp/compress_binlog.lock
+echo "Starting $(date)"
+if [ -f $tmpf ]
+then
+	echo "Already in progress, exiting"
+	exit 0
+fi
+trap 'rm -f $tmpf' 0
+touch $tmpf
 base=$(basename $(grep ^log_bin /etc/my.cnf | cut -d"=" -f 2))
 min_pos=1000000
-echo "Starting $(date)"
+sc=0
 for slave in $(echo "select substring_index(host, ':', 1)  from information_schema.processlist where command = 'binlog dump'" | mysql -ANr -h $MASTER_HOST -u $REPL_USER -p$REPL_PASSWORD 2>/dev/null)
 do
 	if [ -s /tmp/slave_$slave.pos ]	# check if there is an uploaded position first
 	then
 		sp=$(cat /tmp/slave_$slave.pos)
 	else
-		sp=$(echo "show slave status\G" | mysql -Ar -h $slave -u $REPL_USER -p$REPL_PASSWORD 2>/dev/null | fgrep Relay_Master_Log_File | cut -d ":" -f 2)
+		sp=$(echo "show slave status\G" | mysql -Ar -h $slave -u $REPL_USER -p$REPL_PASSWORD 2>/dev/null | fgrep " Master_Log_File" | cut -d ":" -f 2)
 	fi
-	echo "* slave $slave has binlog $sp"
-	[ ${sp#*.} -lt $min_pos ] && min_pos=${sp#*.}
+	if [ "$sp" != "" ]
+	then
+		echo "* slave $slave last downloaded binlog $sp"
+		[ ${sp#*.} -lt $min_pos ] && min_pos=${sp#*.}
+		sc=$((sc+1))
+	fi
 done
-echo "* all slaves low water mark: $min_pos"
+if [ $sc -eq 0 ]
+then
+	echo "* no slaves detected"
+else
+	echo "* last downloaded file of most lagging slave: $min_pos"
+fi
 cd $BINLOG_DIR
 for f in $(find $BINLOG_DIR -name $base.[0-9]\* -mmin +$MIN_AGE_FOR_COMPRESSION | sort)
 do
--- osc_progress.sh
+++ osc_progress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 #	shows real progress of an online schema change session
-#	when PK is a numeric autoinc column
-#	uses current user to log to the DB; you can specify the user running the OSC as an argument
-#	if different than the user running this script (eg. if you want to run this on a slave)
-#	rpizzi@blackbirdit.com
+#	when PK is a numeric autoinc column (works for partitioned tables too)
+#	uses current user to log to the DB; you can specify the user running the OSC as an argument to the script
+#	if it is different than the user running this script (eg. if you want to run this on a slave)
+#	rpizzi@lastminute.com
+#	v1.2
 #
-echo -n "MySQL Password: "
+echo -n "$USER's MySQL Password: "
 stty -echo
 read PASS
 stty echo
@@ -22,7 +23,7 @@ fi
 echo -n "Getting OSC information... "
 while true
 do
-	row=$(echo "select * from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS | sed -e "s/\`//g")
+	row=$(echo "select * from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | sed -e "s/\`//g")
 	[ "$row" != "" ] && break
 	sleep 1
 done
@@ -31,15 +32,21 @@ table=$(echo $row | grep -oP '(?<=FROM\s)\w+\.\w+')
 table_schema=$(echo $table | cut -d"." -f1)
 table_name=$(echo $table | cut -d"." -f2)
 pk=$(echo $row | grep -oP '(?<=WHERE\s\(\()\w+')
-coltype=$(echo "select data_type from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_name = '"$pk"'" | mysql -AN -u $USER -p$PASS)
+pksize=$(echo "select count(*)  from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_key = 'PRI'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+coltype=$(echo "select data_type from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_name = '"$pk"'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
 case "$coltype" in
 	'int'|'bigint') ;;
 	*) echo "sorry, this script only works when PK is an integer"; exit 1;;
 esac
-target=$(echo "select $pk from $table order by 1 desc limit 1" | mysql -AN -u $USER -p$PASS)
 while true
 do
-	curr=$(echo "select * from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS | sed -e "s/\`//g" | grep -oP '(?<=\s\<\=\s.)\w+')
+	target=$(echo "select $pk from $table order by 1 desc limit 1" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+	if [ $pksize -eq 1 ]
+	then
+		curr=$(echo "select  substring_index(substring_index(right(info, 100), '<= \'', -1), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+	else
+		curr=$(echo "select  substring_index(substring_index(right(info, 200), '= \'', -2), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | cut -d"'" -f 1)
+	fi
 	if [ "$curr" = "" ]
 	then
 		sleep 2
