69335d91d6835dd05ed989a87a416b21b24b4261
Rick Pizzi <pizzi@leopardus.com>
2018-05-14 12:50:57 +0200

    added MariaDB compatibility

--- osc_progress.sh
+++ osc_progress.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#	shows real progress of an online schema change session
-#	works when PK is a numeric autoinc column
-#	uses current user to log to the DB; you can specify the user running the OSC as an argument,
-#	if different than the user running this script (eg. if you want to run this on a slave)
-#	rpizzi@blackbirdit.com
+#   shows real progress of an online schema change session
+#   works when PK is a numeric autoinc column
+#   uses current user to log to the DB; you can specify the user running the OSC as an argument,
+#   if different than the user running this script (eg. if you want to run this on a slave)
+#   rick.pizzi@mariadb.com
 #
 echo -n "$USER's MySQL Password: "
 stty -echo
@@ -14,45 +14,46 @@ echo
 mysqladmin -u $USER -p$PASS ping 2>/dev/null | fgrep -q alive
 if [ $? -ne 0 ]
 then
-	echo "invalid password"
-	exit 1
-fi
-[ "$1" != "" ] && OSC_USER=$1 || OSC_USER=$USER
-#
-echo -n "Getting OSC information... "
-while true
-do
-	row=$(echo "select * from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | sed -e "s/\`//g")
-	[ "$row" != "" ] && break
-	sleep 1
-done
-echo "done."
-table=$(echo $row | grep -oP '(?<=FROM\s)\w+\.\w+')
-table_schema=$(echo $table | cut -d"." -f1)
-table_name=$(echo $table | cut -d"." -f2)
-pk=$(echo $row | grep -oP '(?<=WHERE\s\(\()\w+')
-pksize=$(echo "select count(*)  from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_key = 'PRI'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
-coltype=$(echo "select data_type from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_name = '"$pk"'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
-case "$coltype" in
-	'int'|'bigint') ;;
-	*) echo "sorry, this script only works when PK is an integer"; exit 1;;
-esac
-while true
-do
-	target=$(echo "select $pk from $table order by 1 desc limit 1" | mysql -AN -u $USER -p$PASS 2>/dev/null)
-	if [ $pksize -eq 1 ]
-	then
-		curr=$(echo "select  substring_index(substring_index(right(info, 100), '<= \'', -1), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
-	else
-		curr=$(echo "select  substring_index(substring_index(right(info, 200), '= \'', -2), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | cut -d"'" -f 1)
-	fi
-	if [ "$curr" = "" ]
-	then
-		sleep 2
-	else
-		perc=$(echo "scale=4; $curr / $target *100" | bc)
-		/usr/bin/printf "%s: %s/%s (%.2f%%)\n" $table $curr $target $perc
-		sleep 30
-	fi
-done
+        echo "invalid password"
+            exit 1
+        fi
+        [ "$1" != "" ] && OSC_USER=$1 || OSC_USER=$USER
+        #
+        echo -n "Getting OSC information... "
+        while true
+        do
+                row=$(echo "select * from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | sed -e "s/\`//g")
+                    [ "$row" != "" ] && break
+                        sleep 1
+        done
+     echo "done."
+    table=$(echo $row | grep -oP '(?<=FROM\s)\w+\.\w+' | uniq)
+    table_schema=$(echo $table | cut -d"." -f1)
+    table_name=$(echo $table | cut -d"." -f2)
+    pk=$(echo $row | grep -oP '(?<=WHERE\s\(\()\w+' | uniq)
+    pksize=$(echo "select count(*)  from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_key = 'PRI'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+    coltype=$(echo "select data_type from information_schema.columns where table_schema = '"$table_schema"' and table_name = '"$table_name"' and column_name = '"$pk"'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+    case "$coltype" in
+        'int'|'bigint') ;;
+        *) echo "sorry, this script only works when PK is an integer"; exit 1;;
+    esac
+    while true
+    do
+        target=$(echo "select $pk from $table order by 1 desc limit 1" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+        if [ $pksize -eq 1 ]
+        then
+            curr=$(echo "select  substring_index(substring_index(right(info, 100), '<= \'', -1), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null)
+        else
+        curr=$(echo "select  substring_index(substring_index(right(info, 200), '= \'', -2), '\')', 1)  from information_schema.processlist where user = '"$OSC_USER"' and info like 'INSERT LOW%'" | mysql -AN -u $USER -p$PASS 2>/dev/null | cut -d"'" -f 1)
+        fi
+        if [ "$curr" = "" ]
+        then
+            sleep 2
+        else
+            perc=$(echo "scale=4; $curr / $target *100" | bc)
+            /usr/bin/printf "%s: %s/%s (%.2f%%)\n" $table $curr $target $perc
+            sleep 30
+        fi
+    done
 exit 0
+
