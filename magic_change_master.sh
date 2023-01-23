6094951ac5c47d6e557ccebbffd4b821045e0c31
Rick Pizzi <rick.pizzi@mariadb.com>
2023-01-23 11:55:10 +0100

    added ssh option to ignore stdin to avoid hanging in some situations

--- magic_change_master.sh
+++ magic_change_master.sh
@@ -13,7 +13,7 @@ then
 fi
 new_master=$(host $2 | fgrep address | cut -d " " -f 4)
 [ "$new_master" = "" ] && new_master=$2
-curpos=($(echo "show slave status\G" | ssh -q $1 mysql -Ar | egrep " Master_Host| Master_Log_File| Exec_Master_Log_Pos| Relay_Log_File| Relay_Log_Pos" | cut -d ":"  -f 2))
+curpos=($(echo "show slave status\G" | ssh -tq $1 mysql -Ar | egrep " Master_Host| Master_Log_File| Exec_Master_Log_Pos| Relay_Log_File| Relay_Log_Pos" | cut -d ":"  -f 2))
 master=${curpos[0]}
 if [ "$master" = "$new_master" ]
 then
@@ -25,29 +25,34 @@ relay=${curpos[2]}
 relay_pos=${curpos[3]}
 exec_pos=${curpos[4]}
 echo "On slave $1, current situation is: Master $master, File $binlog, Position $exec_pos"
-binlog_base=$(echo "select @@log_bin_basename" | ssh -q $master mysql -ANr)
-relay_base=$(echo "select @@datadir" | ssh -q $master mysql -ANr)
+binlog_base=$(echo "select @@log_bin_basename" | ssh -tq $master mysql -ANr)
+relay_base=$(echo "select @@datadir" | ssh -tq $master mysql -ANr)
 if [ "$3" = "dead" ]
 then
     echo -n "looking up Xid in relay log for position $exec_pos... "
-    xid=$(ssh -q $1 sudo mysqlbinlog $relay_base/$relay | fgrep "end_log_pos $exec_pos " | cut -d"=" -f 2 | tr -d " ")
+    xid=$(ssh -tnq $1 sudo mysqlbinlog $relay_base/$relay | fgrep "end_log_pos $exec_pos " | cut -d"=" -f 2 | tr -d " ")
 else
     echo -n "looking up Xid in $binlog for position $exec_pos... "
-    xid=$(ssh -q $master sudo mysqlbinlog $(dirname $binlog_base)/$binlog | fgrep "end_log_pos $exec_pos " | cut -d"=" -f 2 | tr -d " ")
+    xid=$(ssh -tnq $master sudo mysqlbinlog $(dirname $binlog_base)/$binlog | fgrep "end_log_pos $exec_pos " | cut -d"=" -f 2 | tr -d " ")
 fi
 echo $xid
-binlog_base=$(echo "select @@log_bin_basename" | ssh -q $new_master mysql -ANr)
+binlog_base=$(echo "select @@log_bin_basename" | ssh -tq $new_master mysql -ANr)
 echo -n "searching binlogs on $2 using Xid range search... "
-for b in $(ssh -q $new_master ls -t $binlog_base*.[0-9]*)
+for b in $(ssh -tq $new_master ls -t $binlog_base*.[0-9]*)
 do
     new_pos_binlog=$(basename $b)
     echo -n $new_pos_binlog" "
-    bxid=$(ssh -q $new_master sudo mysqlbinlog $b | fgrep  -m 1 "Xid = " | cut -d "=" -f 2 | tr -d " ")
+    bxid=$(ssh -tnq $new_master sudo mysqlbinlog $b | fgrep  -m 1 "Xid = " | cut -d "=" -f 2 | tr -d " ")
+    if [ "$bxid" = "" ] 
+    then
+	echo " FAILED! Check that passwordless sudo is enabled!"
+	exit 1
+    fi
     [ $bxid -lt $xid ] && break
 done
 echo
 echo -n "looking up position in $new_pos_binlog for Xid $xid... "
-new_pos=$(ssh -q $new_master sudo mysqlbinlog $b | fgrep "Xid = $xid" | tr -s " " | cut -d " " -f 7)
+new_pos=$(ssh -tnq $new_master sudo mysqlbinlog $b | fgrep "Xid = $xid" | tr -s " " | cut -d " " -f 7)
 echo $new_pos
 echo "All done! Connect to $1 and issue: CHANGE MASTER TO MASTER_HOST='$new_master', MASTER_PORT=3306, MASTER_LOG_FILE='$new_pos_binlog', MASTER_LOG_POS=$new_pos"
 exit 0
